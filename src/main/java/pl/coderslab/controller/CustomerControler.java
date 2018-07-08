68473c6f00b6229bc0f69b4dd089d59463524d1f
ciemic <michal.cieslar@gmail.com>
2018-07-08 20:07:28 +0200

    added vehicles list for customers

--- src/main/java/pl/coderslab/controller/CustomerControler.java
+++ src/main/java/pl/coderslab/controller/CustomerControler.java
@@ -2,8 +2,10 @@ package pl.coderslab.controller;
 
 import pl.coderslab.dao.CustomerDao;
 import pl.coderslab.dao.EmployeeDao;
+import pl.coderslab.dao.VehicleDao;
 import pl.coderslab.model.Customer;
 import pl.coderslab.model.Employee;
+import pl.coderslab.model.Vehicle;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +13,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @WebServlet(name = "CustomerControler", urlPatterns = "/customer")
 public class CustomerControler extends HttpServlet {
@@ -22,12 +28,15 @@ public class CustomerControler extends HttpServlet {
 
         int customerId;
         Customer customer = new Customer();
+        List<Vehicle> vehicleList = new ArrayList<>();
         try {
             customerId = Integer.parseInt(request.getParameter("id"));
             customer = CustomerDao.loadCustomerById(customerId);
+            vehicleList = VehicleDao.loadAllCustomerVehicles(customerId);
         } catch (Exception e) {
         }
         request.setAttribute("customer", customer);
+        request.setAttribute("vehicles", vehicleList);
 
         getServletContext().getRequestDispatcher("/WEB-INF/customer.jsp").forward(request, response);
     }
